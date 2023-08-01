f11bd9f17e07e73d11cc1e73af9dad972a27bc7c
emmywh <white.e855@gmail.com>
2023-07-31 22:05:57 -0600

    db_test route added

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("your_db_url_here")
+    conn.close
+    return "Database Connection Successful"
